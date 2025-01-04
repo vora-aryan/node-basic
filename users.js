5e09815d616dcce037c894c51e50cc15b9dd7fef
Aryan <aryanvora619@gmail.com>
2025-01-04 16:47:11 +0530

    user js changed

--- users.js
+++ users.js
@@ -3,8 +3,7 @@ const userAge = 20;
 
 const printUser = (name, age) => {
   console.log(userName, age);
-  // console.log(userName, age);
-
+  console.log("Hello ");
 };
 
 module.exports = { userName, userAge, printUser };
